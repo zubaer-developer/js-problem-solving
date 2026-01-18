321688ccf2eb94457bc05028843daa771853a773
ZUBAER <zubaer.developer@gmail.com>
2026-01-18 17:24:39 +0600

    PingPong Problem Solved

---
+++ problem10_pingpong.js
@@ -0,0 +1,15 @@
+function pingPong() {
+  for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("PingPong");
+    } else if (i % 3 === 0) {
+      console.log("Ping");
+    } else if (i % 5 === 0) {
+      console.log("Pong");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+pingPong();
