84d4d06da734e4b0282eec8c3d2bf316f7c6bb1e
ZUBAER <zubaer.developer@gmail.com>
2026-01-18 16:56:51 +0600

    find even numbers Problem Solved

---
+++ problem7_findEvenNumbers.js
@@ -0,0 +1,12 @@
+function findEvenNumbers(arr) {
+  let evenNumbers = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evenNumbers.push(arr[i]);
+    }
+  }
+  return evenNumbers;
+}
+
+console.log(findEvenNumbers([1, 2, 3, 4, 5, 6]));
