ca29f0c4c203e164c368267cf9fd7e87903d74a5
ZUBAER <zubaer.developer@gmail.com>
2026-01-18 16:50:15 +0600

    Find Max Number Problem Solved

---
+++ problem4_findMax.js
@@ -0,0 +1,12 @@
+function findMax(numbers) {
+  let max = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+  return max;
+}
+
+console.log(findMax([5, 1, 9, 3]));
